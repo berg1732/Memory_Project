6a77f84e2288b29938356a835e73492b3ffd4c27
kris van den Berg <krisberg@home.nl>
2017-10-13 15:41:41 +0200

    fix imgs

--- Memory_Project/Game.Designer.cs
+++ Memory_Project/Game.Designer.cs
@@ -84,136 +84,153 @@
             this.panel1.Controls.Add(this.Card3);
             this.panel1.Controls.Add(this.Card2);
             this.panel1.Controls.Add(this.Card1);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(16, 15);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(439, 630);
+            this.panel1.Size = new System.Drawing.Size(585, 775);
             this.panel1.TabIndex = 0;
             // 
             // Card16
             // 
-            this.Card16.Location = new System.Drawing.Point(321, 471);
+            this.Card16.Location = new System.Drawing.Point(428, 580);
+            this.Card16.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Card16.Name = "Card16";
-            this.Card16.Size = new System.Drawing.Size(100, 150);
+            this.Card16.Size = new System.Drawing.Size(133, 185);
             this.Card16.TabIndex = 15;
             this.Card16.TabStop = false;
             // 
             // Card15
             // 
-            this.Card15.Location = new System.Drawing.Point(215, 471);
+            this.Card15.Location = new System.Drawing.Point(287, 580);
+            this.Card15.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Card15.Name = "Card15";
-            this.Card15.Size = new System.Drawing.Size(100, 150);
+            this.Card15.Size = new System.Drawing.Size(133, 185);
             this.Card15.TabIndex = 14;
             this.Card15.TabStop = false;
             // 
             // Card14
             // 
-            this.Card14.Location = new System.Drawing.Point(109, 471);
+            this.Card14.Location = new System.Drawing.Point(145, 580);
+            this.Card14.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Card14.Name = "Card14";
-            this.Card14.Size = new System.Drawing.Size(100, 150);
+            this.Card14.Size = new System.Drawing.Size(133, 185);
             this.Card14.TabIndex = 13;
             this.Card14.TabStop = false;
             // 
             // Card13
             // 
-            this.Card13.Location = new System.Drawing.Point(3, 471);
+            this.Card13.Location = new System.Drawing.Point(4, 580);
+            this.Card13.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Card13.Name = "Card13";
-            this.Card13.Size = new System.Drawing.Size(100, 150);
+            this.Card13.Size = new System.Drawing.Size(133, 185);
             this.Card13.TabIndex = 12;
             this.Card13.TabStop = false;
             // 
             // Card12
             // 
-            this.Card12.Location = new System.Drawing.Point(321, 315);
+            this.Card12.Location = new System.Drawing.Point(428, 388);
+            this.Card12.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Card12.Name = "Card12";
-            this.Card12.Size = new System.Drawing.Size(100, 150);
+            this.Card12.Size = new System.Drawing.Size(133, 185);
             this.Card12.TabIndex = 11;
             this.Card12.TabStop = false;
             // 
             // Card11
             // 
-            this.Card11.Location = new System.Drawing.Point(215, 315);
+            this.Card11.Location = new System.Drawing.Point(287, 388);
+            this.Card11.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Card11.Name = "Card11";
-            this.Card11.Size = new System.Drawing.Size(100, 150);
+            this.Card11.Size = new System.Drawing.Size(133, 185);
             this.Card11.TabIndex = 10;
             this.Card11.TabStop = false;
             // 
             // Card10
             // 
-            this.Card10.Location = new System.Drawing.Point(109, 315);
+            this.Card10.Location = new System.Drawing.Point(145, 388);
+            this.Card10.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Card10.Name = "Card10";
-            this.Card10.Size = new System.Drawing.Size(100, 150);
+            this.Card10.Size = new System.Drawing.Size(133, 185);
             this.Card10.TabIndex = 9;
             this.Card10.TabStop = false;
             // 
             // Card9
             // 
-            this.Card9.Location = new System.Drawing.Point(3, 315);
+            this.Card9.Location = new System.Drawing.Point(4, 388);
+            this.Card9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Card9.Name = "Card9";
-            this.Card9.Size = new System.Drawing.Size(100, 150);
+            this.Card9.Size = new System.Drawing.Size(133, 185);
             this.Card9.TabIndex = 8;
             this.Card9.TabStop = false;
             // 
             // Card8
             // 
-            this.Card8.Location = new System.Drawing.Point(321, 159);
+            this.Card8.Location = new System.Drawing.Point(428, 196);
+            this.Card8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Card8.Name = "Card8";
-            this.Card8.Size = new System.Drawing.Size(100, 150);
+            this.Card8.Size = new System.Drawing.Size(133, 185);
             this.Card8.TabIndex = 7;
             this.Card8.TabStop = false;
             // 
             // Card7
             // 
-            this.Card7.Location = new System.Drawing.Point(215, 159);
+            this.Card7.Location = new System.Drawing.Point(287, 196);
+            this.Card7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Card7.Name = "Card7";
-            this.Card7.Size = new System.Drawing.Size(100, 150);
+            this.Card7.Size = new System.Drawing.Size(133, 185);
             this.Card7.TabIndex = 6;
             this.Card7.TabStop = false;
             // 
             // Card6
             // 
-            this.Card6.Location = new System.Drawing.Point(109, 159);
+            this.Card6.Location = new System.Drawing.Point(145, 196);
+            this.Card6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Card6.Name = "Card6";
-            this.Card6.Size = new System.Drawing.Size(100, 150);
+            this.Card6.Size = new System.Drawing.Size(133, 185);
             this.Card6.TabIndex = 5;
             this.Card6.TabStop = false;
             // 
             // Card5
             // 
-            this.Card5.Location = new System.Drawing.Point(3, 159);
+            this.Card5.Location = new System.Drawing.Point(4, 196);
+            this.Card5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Card5.Name = "Card5";
-            this.Card5.Size = new System.Drawing.Size(100, 150);
+            this.Card5.Size = new System.Drawing.Size(133, 185);
             this.Card5.TabIndex = 4;
             this.Card5.TabStop = false;
             // 
             // Card4
             // 
-            this.Card4.Location = new System.Drawing.Point(321, 3);
+            this.Card4.Location = new System.Drawing.Point(428, 4);
+            this.Card4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Card4.Name = "Card4";
-            this.Card4.Size = new System.Drawing.Size(100, 150);
+            this.Card4.Size = new System.Drawing.Size(133, 185);
             this.Card4.TabIndex = 3;
             this.Card4.TabStop = false;
             // 
             // Card3
             // 
-            this.Card3.Location = new System.Drawing.Point(215, 3);
+            this.Card3.Location = new System.Drawing.Point(287, 4);
+            this.Card3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Card3.Name = "Card3";
-            this.Card3.Size = new System.Drawing.Size(100, 150);
+            this.Card3.Size = new System.Drawing.Size(133, 185);
             this.Card3.TabIndex = 2;
             this.Card3.TabStop = false;
             // 
             // Card2
             // 
-            this.Card2.Location = new System.Drawing.Point(110, 3);
+            this.Card2.Location = new System.Drawing.Point(147, 4);
+            this.Card2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Card2.Name = "Card2";
-            this.Card2.Size = new System.Drawing.Size(100, 150);
+            this.Card2.Size = new System.Drawing.Size(133, 185);
             this.Card2.TabIndex = 1;
             this.Card2.TabStop = false;
             // 
             // Card1
             // 
-            this.Card1.Location = new System.Drawing.Point(3, 3);
+            this.Card1.Location = new System.Drawing.Point(4, 4);
+            this.Card1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Card1.Name = "Card1";
-            this.Card1.Size = new System.Drawing.Size(100, 150);
+            this.Card1.Size = new System.Drawing.Size(133, 185);
             this.Card1.TabIndex = 0;
             this.Card1.TabStop = false;
             // 
@@ -225,16 +242,18 @@
             // 
             // Game
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1150, 705);
+            this.ClientSize = new System.Drawing.Size(1533, 868);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Game";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Game";
+            this.Load += new System.EventHandler(this.GameWindow_Load);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.Card16)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Card15)).EndInit();
---
+++ Memory_Project/startGame.Designer.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Memory_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(startGame));
+            this.buttonStartGame = new System.Windows.Forms.Button();
+            this.buttonLeaderboard = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStartGame
+            // 
+            this.buttonStartGame.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonStartGame.Location = new System.Drawing.Point(387, 45);
+            this.buttonStartGame.Name = "buttonStartGame";
+            this.buttonStartGame.Size = new System.Drawing.Size(75, 23);
+            this.buttonStartGame.TabIndex = 0;
+            this.buttonStartGame.Text = "Start Game";
+            this.buttonStartGame.UseVisualStyleBackColor = false;
+            this.buttonStartGame.Click += new System.EventHandler(this.buttonStartGame_Click);
+            // 
+            // buttonLeaderboard
+            // 
+            this.buttonLeaderboard.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonLeaderboard.Location = new System.Drawing.Point(387, 75);
+            this.buttonLeaderboard.Name = "buttonLeaderboard";
+            this.buttonLeaderboard.Size = new System.Drawing.Size(75, 23);
+            this.buttonLeaderboard.TabIndex = 1;
+            this.buttonLeaderboard.Text = "Leaderboard";
+            this.buttonLeaderboard.UseVisualStyleBackColor = false;
+            // 
+            // startGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 511);
+            this.Controls.Add(this.buttonLeaderboard);
+            this.Controls.Add(this.buttonStartGame);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "startGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Memory Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStartGame;
+        private System.Windows.Forms.Button buttonLeaderboard;
+    }
+}
+
