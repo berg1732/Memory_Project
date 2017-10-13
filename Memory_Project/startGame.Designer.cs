4d13f27346a110678cb1cbd13b7821edfaacea26
kris van den Berg <krisberg@home.nl>
2017-10-13 14:33:13 +0200

    fix resizable from

--- Memory_Project/startGame.Designer.cs
+++ Memory_Project/startGame.Designer.cs
@@ -36,9 +36,10 @@
             // buttonStartGame
             // 
             this.buttonStartGame.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.buttonStartGame.Location = new System.Drawing.Point(387, 45);
+            this.buttonStartGame.Location = new System.Drawing.Point(516, 55);
+            this.buttonStartGame.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonStartGame.Name = "buttonStartGame";
-            this.buttonStartGame.Size = new System.Drawing.Size(75, 23);
+            this.buttonStartGame.Size = new System.Drawing.Size(100, 28);
             this.buttonStartGame.TabIndex = 0;
             this.buttonStartGame.Text = "Start Game";
             this.buttonStartGame.UseVisualStyleBackColor = false;
@@ -46,23 +47,28 @@
             // buttonLeaderboard
             // 
             this.buttonLeaderboard.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.buttonLeaderboard.Location = new System.Drawing.Point(387, 75);
+            this.buttonLeaderboard.Location = new System.Drawing.Point(516, 92);
+            this.buttonLeaderboard.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonLeaderboard.Name = "buttonLeaderboard";
-            this.buttonLeaderboard.Size = new System.Drawing.Size(75, 23);
+            this.buttonLeaderboard.Size = new System.Drawing.Size(100, 28);
             this.buttonLeaderboard.TabIndex = 1;
             this.buttonLeaderboard.Text = "Leaderboard";
             this.buttonLeaderboard.UseVisualStyleBackColor = false;
             // 
             // startGame
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Memory_Project.Properties.Resources.startFormBG;
-            this.ClientSize = new System.Drawing.Size(509, 511);
+            this.ClientSize = new System.Drawing.Size(679, 629);
             this.Controls.Add(this.buttonLeaderboard);
             this.Controls.Add(this.buttonStartGame);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
             this.Name = "startGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Memory Game";
             this.ResumeLayout(false);
 
