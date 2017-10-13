6446b4907f575a090a91eeccf3258a93fca1bf95
Albert van der Berg <berg1732@student.nhl.nl>
2017-10-13 14:22:50 +0200

    Background + Icon + Size

--- Memory_Project/startGame.Designer.cs
+++ Memory_Project/startGame.Designer.cs
@@ -28,9 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(startGame));
+            this.SuspendLayout();
+            // 
+            // startGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackgroundImage = global::Memory_Project.Properties.Resources.startFormBG;
+            this.ClientSize = new System.Drawing.Size(509, 511);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "startGame";
+            this.Text = "Memory Game";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
