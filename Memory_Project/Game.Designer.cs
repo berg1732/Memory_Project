286ea0a08516794c94dac569f4f09572229d096e
kris van den Berg <krisberg@home.nl>
2017-10-16 12:14:30 +0200

    fix

--- Memory_Project/Game.Designer.cs
+++ Memory_Project/Game.Designer.cs
@@ -81,7 +81,7 @@
             // 
             this.lblShowImages.AutoSize = true;
             this.lblShowImages.Font = new System.Drawing.Font("Modern No. 20", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblShowImages.Location = new System.Drawing.Point(647, 114);
+            this.lblShowImages.Location = new System.Drawing.Point(645, 182);
             this.lblShowImages.Name = "lblShowImages";
             this.lblShowImages.Size = new System.Drawing.Size(23, 25);
             this.lblShowImages.TabIndex = 18;
