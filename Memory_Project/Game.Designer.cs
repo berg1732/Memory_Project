4dc78e17ebbc807e63153c4fa8ae6197c3f7bf4e
ThePenguinDuck <marcopiest@gmail.com>
2017-10-19 14:09:20 +0200

    bug fix size

--- Memory_Project/Game.Designer.cs
+++ Memory_Project/Game.Designer.cs
@@ -314,6 +314,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(1102, 1038);
             this.Controls.Add(this.button_LoadGame);
             this.Controls.Add(this.button_SaveGame);
